fba8af558d9bb3e04a279bd14ca1ffea3bfafc13
Christopher Dorst <chris@dorst.email>
2018-03-25 16:50:35 -0500

    Update TableReferenceGetterOrCreator.cs

--- Azure.Storage.Table.GetOrCreateTableReference/TableReferenceGetterOrCreator.cs
+++ Azure.Storage.Table.GetOrCreateTableReference/TableReferenceGetterOrCreator.cs
@@ -1,3 +1,6 @@
+// Copyright © Christopher Dorst. All rights reserved.
+// Licensed under the GNU General Public License, Version 3.0. See the LICENSE document in the repository root for license information.
+
 using Microsoft.WindowsAzure.Storage.Table;
 using System.Threading.Tasks;
 using static Azure.Storage.Table.GetTableReference.TableReferenceGetter;
@@ -10,9 +13,9 @@ namespace Azure.Storage.Table.GetOrCreateTableReference
         /// <summary>Returns a reference of a new or existing Microsoft Azure CloudTable using the given connection string and table name</summary>
         public static async Task<CloudTable> GetOrCreateAzureTable(string connectionString, string tableName)
         {
-var table = TableReference(connectionString, tableName);
-await table.CreateIfNotExistsAsync();
-return table;;
+            var table = TableReference(connectionString, tableName);
+            await table.CreateIfNotExistsAsync();
+            return table;;
         }
     }
 }
